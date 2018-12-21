fb764a9ffcae52cf4bfcc24412f24e5829f92488
dhope-nagesh <dhope.nagesh@gmail.com>
2018-12-21 21:59:29 +0530

    Puzzle Deployment

---
+++ puzzle/precache-manifest.5c6b9affde4fb193ce1da96ede0cf308.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56a16b96c35122424566",
+    "url": "/static/css/main.e6857016.chunk.css"
+  },
+  {
+    "revision": "56a16b96c35122424566",
+    "url": "/static/js/main.56a16b96.chunk.js"
+  },
+  {
+    "revision": "d3003d29c96c5897264a",
+    "url": "/static/js/1.d3003d29.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3f64b344c7baddeb431d61d60f7e61ea",
+    "url": "/index.html"
+  }
+];
